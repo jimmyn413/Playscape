2e70d584917e08330dcd951e2d9730deb8c79a38
Nancy Nguyen <nancyktnguyen@gmail.com>
2016-10-05 00:02:18 -0700

    EventsController, Models, EventsService

---
+++ Playscape/Models/Requests/EventsUpdateRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playscape.Models.Requests
+{
+    public class EventsUpdateRequest
+    {
+        public int Id { get; set; }
+
+        public int EventType { get; set; }
+
+        public bool IsPublic { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public int Duration { get; set; }
+
+        public int MediaId { get; set; }
+
+        public int NumberAttendees { get; set; }
+
+    }
+}
---
+++ Playscape/Models/Responses/PagedItemsResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sabio.Web.Models.Responses
+{
+    public class PagedItemsResponse<T> : ItemsResponse<T>
+    {
+        public int PageNumber {get;set;}
+
+        public int PageSize { get; set; }
+
+        public int TotalItems { get; set; }
+
+        public string QueryString { get; set; }
+
+        public Guid UserId { get; set; }
+
+    }
+}
