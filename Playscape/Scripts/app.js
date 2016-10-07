4b4bf332d45d471b8071da8aaa5f79c9ee152216
Jimmy <jimmytrannguyen@gmail.com>
2016-10-07 11:42:04 -0700

    eveyrthingworking,nocal

--- Playscape/Scripts/app.js
+++ Playscape/Scripts/app.js
@@ -1,29 +1,3 @@
 /** *************Angular app JS*********************/
 "use strict"; 
-var app = angular.module('contactApp', ['mwl.calendar', 'ngAnimate', 'ui.bootstrap']);
-
-angular.module('contactApp')
-      .config(function (calendarConfig) {
-
-          calendarConfig.dateFormatter = 'moment'; // use moment to format dates
-
-      })
-      .factory('alert', function($uibModal) {
-
-          function show(action, event) {
-              return $uibModal.open({
-                  templateUrl: '/Templates/modalContent.html',
-                  controller: function() {
-                      var vm = this;
-                      vm.action = action;
-                      vm.event = event;
-                  },
-                  controllerAs: 'vm'
-              });
-          }
-
-          return {
-              show: show
-          };
-
-      });
+var app = angular.module('contactApp', ['ngAnimate']);
